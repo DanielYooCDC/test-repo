15b3e49a3eb6c20def4b0f3db178aa76a9a06b99
Young Moo (Daniel) Yoo <danielyoo1112@gmail.com>
2026-01-19 16:53:36 -0500

    Revert "change age mean"
    This reverts commit 6851a9fac3cbb54aacd8c78005fb3734358c7374.

--- test-r.R
+++ test-r.R
@@ -5,7 +5,7 @@ set.seed(123)  # for reproducibility
 # --- Simulate raw data ---
 raw_data <- tibble(
     id = 1:100,
-    age = rnorm(100, mean = 55, sd = 15),
+    age = rnorm(100, mean = 45, sd = 15),
     bmi = rnorm(100, mean = 27, sd = 4),
     systolic_bp = rnorm(100, mean = 120, sd = 15)
 )
